01b7871ea2e66867c5a2c44a05ebcc5228132db9
Kim Reyes <reyeskimberly018@gmail.com>
2023-07-18 17:42:59 +0800

    Add test case for admin modules v8

--- lambda/STARK_CodeGen_Dynamic/cgdynamic_buildspec/__init__.py
+++ lambda/STARK_CodeGen_Dynamic/cgdynamic_buildspec/__init__.py
@@ -33,6 +33,7 @@ def create(data):
                 - aws s3 sync static s3://$WEBSITE --delete --acl public-read
                 - aws s3 sync lambda/packaged_layers s3://$BUCKET/{project_varname}/STARKLambdaLayers --delete --exclude="*" --include="*.zip"
                 - aws s3 cp outputtemplate.yml s3://$BUCKET/{project_varname}/
+                - cp -r lambda/test_cases lambda/
 
         artifacts:
             files:
