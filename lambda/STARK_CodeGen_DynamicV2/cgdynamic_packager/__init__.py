6ff2513c4b20208d35ae0f41e31466e69a73195f
Mark Danico A. Fernandez <nico@serverless.ninja>
2023-08-15 12:52:49 +0800

    fix key name of object for static site url

--- lambda/STARK_CodeGen_DynamicV2/cgdynamic_packager/__init__.py
+++ lambda/STARK_CodeGen_DynamicV2/cgdynamic_packager/__init__.py
@@ -145,7 +145,7 @@ def get_terraform_output_static_site_url():
     response = s3.put_object(
         Body=tf_output,
         Bucket=codegen_bucket_name,
-        Key=f'l{{project_varname}}/static_site_url.txt',
+        Key=f'codegen_dynamic/{{project_varname}}/static_site_url.txt',
         Metadata={{
             'STARK_Description': 'static site url fetching for deployment'
         }}
